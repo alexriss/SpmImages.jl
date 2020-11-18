7e650b612bf58dbe313a20b81c82ca59762e261f
alexriss <00alexx@riss.at>
2020-11-18 15:47:25 +0100

    added tests; updated readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SpmImages
+# SpmImages.jl
 
 A julia library to read and display SPM (scanning probe microscopy) images. Currently, only Nanonis files are supported.
 
@@ -6,3 +6,4 @@ The commands are somewhat similar to the ones in the respective python version [
 ## Related projects:
 
 - [imag*ex*](https://github.com/alexriss/imagex): Python scripts to analyze scanning probe images.
+- [grid*ex*](https://github.com/alexriss/gridex): Python scripts to analyze 3D grid data.
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using SpmImages
 using Test
 
 @testset "SpmImages.jl" begin
-    # Write your tests here.
+    ima = load_image("Image_445.sxm")
+    @test ima.channel_names == ["Z", "LI_X", "LI_Y", "Bias", "Current", "Phase", "Amplitude", "Frequency_Shift", "Excitation"]
+    @test ima.channel_units == ["m", "V", "V", "V", "A", "deg", "m", "Hz", "V"]
+    @test ima.scansize == [2.0, 4.0]
+    @test ima.scansize_unit == "nm"
+    @test ima.pixelsize == [96, 192]
+    @test ima.scan_direction == SpmImages.up
+    @test ima.acquisition_time == 4877.1
+    @test ima.header["oscillation_control>reference_phase_(deg)"] == "65.16E+0"
+    @test ima.data[12,42,3] == -3.6583905f0
+    @test ima.data[9,18,10] == 3.0707468f-11
+    @test ima.data[69,69,6] == -0.7235545f0
 end
